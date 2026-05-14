578fad9f7009be500e3cf7d84492ca0567776331
ThomasMSHKV <tmereshkov@gmail.com>
2026-05-14 19:11:09 +0300

    other screen

--- lib/features/profile/presentation/screens/profile_screens.dart
+++ lib/features/profile/presentation/screens/profile_screens.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:VayToday/features/auth/presentation/screens/login_screen.dart';
+import 'package:VayToday/features/other/presentation/screens/other_screen.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -8,10 +9,34 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (!_isAuthorized) {
-      return const LoginScreen();
-    }
+    return Stack(
+      children: [
+        if (!_isAuthorized)
+          const LoginScreen()
+        else
+          const Scaffold(body: Center(child: Text('Профиль'))),
 
-    return const Scaffold(body: Center(child: Text('Профиль')));
+        SafeArea(
+          child: Align(
+            alignment: Alignment.topRight,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 18, right: 22),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (_) => const OtherScreen()),
+                  );
+                },
+                child: const Icon(
+                  Icons.menu_rounded,
+                  size: 42,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
